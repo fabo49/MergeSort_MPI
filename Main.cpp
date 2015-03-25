0f51d82c7684c2beb8a3b33cb17b612dcf610960
fabo49 <farodriguez.49@gmail.com>
2015-03-25 15:19:52 -0600

    Ingreso de datos por parte del usuario

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,32 @@
 #include <stdlib.h>
 #include <string>
 #include "mpi.h"
+#include "ordenador.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
 
+    int numProcesos;
+    int numElementos;
+    bool compatibles = false;
+    while(compatibles == false){
+        cout<<"Ingrese la cantidad de elementos que quiere en el vector: ";
+        cin>>numElementos;
+        cout<<"\nIngrese la cantidad de procesos que quiere crear para correr el programa: ";
+        cin>>numProcesos;
+        cout<<endl;
+        if(numElementos % numProcesos == 0){
+            compatibles = true;
+        }else{
+            cout<<"Los datos ingresados no funcionan, la cantidad de elementos tiene que ser"<<endl;
+            cout<<"divisible entre la cantidad de procesos. Vuelva a ingresar los datos"<<endl;
+            cout<<"------------------------------------------------------------------------"<<endl;
+        }
+    }
+
+    ordenador metodoOrde;
     MPI_Init(&argc, &argv);                                               // Arranque del proceso MPI
 
 
@@ -27,7 +47,7 @@ int main(int argc, char **argv)
 
     if(my_rank==0){                                                      // Proceso raiz 0
 
-        for(int i = 0; i < 128; ++i){                                   // Se generan todos los numeros random 
+        for(int i = 0; i < 128; ++i){                                   // Se generan todos los numeros random
             valor = (i* (rand()%10+1) );
             arrayDesorden[i] = valor;
             numeros = to_string(valor);
@@ -40,7 +60,8 @@ int main(int argc, char **argv)
     }else{
         
         MPI_Recv(arrayTemporal, 16, MPI_INT, 0, 911, MPI_COMM_WORLD, &status);      // Cada proceso recibe de la raiz los numeros iniciales a ordenar
-        
+        //MPI_Recv(arrayTemporal, 16, MPI_INT, 0, ANI
+        metodoOrde.mergeSort(arrayTemporal, 16);                             // Envio a ordenar lo que me corresponde a mi
 
 
     }
