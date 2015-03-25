9fefcf567ad1b8ff5f0918d92c529145af1232e5
SlyJose <jpurena14@hotmail.com>
2015-03-24 19:55:38 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string>
-//#include "mpi.h"
+#include "mpi.h"
 
 using namespace std;
 
@@ -14,37 +14,49 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);                                               // Arranque del proceso MPI
 
 
-    int arrayDesorden[128];                                              // Array que almacena los numeros en desorden
+    int arrayDesorden[128];                                              // Array que almacena todos los numeros en desorden
     string numeros = "";
     int valor = 0;
     srand(time(NULL));
 
     ofstream numerosDesorden;                                           // Objeto Archivo
 
-    numerosDesorden.open("ListaI.txt");                                 // Almacena los numeros en desorden
+    numerosDesorden.open("ListaI.txt");                                 // Se crea el archivo que contiene los numeros en desorden
     numerosDesorden << "Números en desorden: " << endl;
 
-    if(my_rank==0){                                                      // Proceso raiz
+    if(my_rank==0){                                                      // Proceso raiz 0
 
-        for(int i = 0; i < 128; ++i){
+        for(int i = 0; i < 128; ++i){                                   // Se generan todos los numeros random 
             valor = (i* (rand()%10+1) );
             arrayDesorden[i] = valor;
             numeros = to_string(valor);
-            numerosDesorden << numeros+" " << endl;                     // Se llena la lista desordenada con valores random
+            numerosDesorden << numeros+" " << endl;                     // Se llena el archivo con valores random
         }
         
-        for(int j = 1; j < 9; ++j){
-            
-        }
+        MPI_Bcast(array, 16, MPI_INT, 0, MPI_COMM_WORLD);             // Encargado de entregar a cada proceso una cierta cantidad equitativa de elementos para que los ordene
 
 
     }else{
+        
+        
 
 
     }
-
-
     MPI_Finalize();                                                     // Finalización del proceso MPI
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
