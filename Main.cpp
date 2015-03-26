8276568519490f06d0de21ca8a87d484ae9162b2
fabo49 <farodriguez.49@gmail.com>
2015-03-25 22:36:57 -0600

    Ya casi
    Se implementó la repartición x parte del proceso 0 y además el proceso
    0 ordena lo que le corresponde.
    Como comentado sale lo que deben de hacer los demás procesos para
    recibir y seguir enviando los subvectores a los demás procesos

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,14 @@
+/**
+  * Universidad de Costa Rica
+  * Escuela de Ciencias de la Computacion e Informatica
+  * Arquitectura de Computadoras
+  * Tarea programada 1
+  * @author Fabian Rodriguez B25695
+  * @author Jose Pablo Ureña B
+  * I Semestre 2015
+  * El programa implementa el algoritmo de merge sort con paralelismo y utilizando MPI para el paso de mensajes
+  */
+
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
@@ -24,7 +35,7 @@ int main(int argc, char **argv)
         cout<<"\nIngrese la cantidad de procesos que quiere crear para correr el programa: ";
         cin>>numProcesos;
         cout<<endl;
-        if( (numElementos % numProcesos == 0) && (numProcesos <= 32) ){
+        if( (numElementos % numProcesos == 0) && (numProcesos <= 32) && (numProcesos > 0) && (numElementos > 0) ){
             compatibles = true;
         }else{
             cout<<"Los datos ingresados no funcionan, la cantidad de elementos tiene que ser"<<endl;
@@ -39,18 +50,23 @@ int main(int argc, char **argv)
     int etiquetaProcesos[etiTamano];                  /* Vector que almacena la etiqueta de los procesos que deben tomar el vector de elementos
                                                             ya ordenados, de otros procesos.*/
     for(int k = 0; k < etiTamano; ++k){               // Se llena el vector con las etiquetas de procesos que reciben por primera vez el vector de elementos ordenado
-        etiquetaProcesos[0] = 2*k;   
+        etiquetaProcesos[0] = 2*k;
     }
+
     tamVecTmp = numElementos/numProcesos;               /* Cuantos elementos le tocan a cada proceso */
-    MPI_Init(&argc, &argv);                                               // Arranque del proceso MPI
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesos);                         /* El comunicador MPI_COMM_WORLD almacena el numero de procesos a ejecutar en la 
+    int* vecTemporal;                                   /* Array de cada proceso */
+    int arrayDesorden[numElementos];                    /* Array que almacena todos los numeros en desorden*/
+    int paso;                                           /* Para indicar en cual iteracion (o nivel del arbol) se encuentra */
+
+    MPI_Init(&argc, &argv);                                               /* Arranque del proceso MPI */
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesos);                         /* El comunicador MPI_COMM_WORLD almacena el numero de procesos a ejecutar en la
                                                                             variable numProcesos */
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);                             /* El comunicador MPI_COMM_WORLD almacena en la variable my_rank la etiqueta del 
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);                             /* El comunicador MPI_COMM_WORLD almacena en la variable my_rank la etiqueta del
                                                                            proceso que realiza el llamado. */
 
     if(my_rank==0){                                                      // Proceso raiz 0
-    
-        int arrayDesorden[numElementos];                                              // Array que almacena todos los numeros en desorden
+
         string numeros = "";
         int valor = 0;
         srand(time(NULL));
@@ -67,24 +83,55 @@ int main(int argc, char **argv)
             numerosDesorden << numeros+" " << endl;                     // Se llena el archivo con valores random
         }
         
-        // Preguntar si puede especificarse que un proceso reciba de solo una etiqueta
+        // ******** Preguntar si puede especificarse que un proceso reciba de solo una etiqueta
         
-        MPI_Scatter(arrayDesorden, tamVecTmp, MPI_INT, );      /* El proceso raiz envia a todos los procesos incluido el mismo, los elementos 
-                                                               a ordenar inicialmente, es decir, numElementos / numProcesos*/
-        
+        MPI_Bcast(&tamVecTmp, 1, MPI_INT, 0, MPI_COMM_WORLD);   /* Se encarga de enviarle a cada proceso el numero de elementos que le tocan a ese proceso.*/
+        vecTemporal = new int[tamVecTmp];//******** ESTO SE PUEDE? *******
+        MPI_Scatter(arrayDesorden, tamVecTmp, MPI_INT, vecTemporal, tamVecTmp, MPI_INT, 0, MPI_COMM_WORLD);  /* El proceso raiz envia a todos los procesos incluido el mismo, los elementos
+                                                                                                                a ordenar inicialmente, es decir, numElementos / numProcesos*/
+        metodoOrde.mergeSort(vecTempral, 0, temVecTmp-1);       /* Ordena la parte que le corresponde al proceso,
+                                                                   esto es el proceso 0.*/
+    }else{
+        MPI_Bcast(&tamVecTmp, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        vecTemporal = new int[tamVecTmp];
+        MPI_Scatter(arrayDesorden, tamVecTmp, vecTemporal, tamVecTmp, MPI_INT, 0, MPI_COMM_WORLD);
     }
     
     // aqui debe hacerse el primer merge en cada proceso
     
-    
+    /*
+    step = 1;
+    while(step<p)
+    {
+        if(id%(2*step)==0)
+        {
+            if(id+step<p)
+            {
+                MPI_Recv(&m,1,MPI_INT,id+step,0,MPI_COMM_WORLD,&status);
+                other = (int *)malloc(m*sizeof(int));
+                MPI_Recv(other,m,MPI_INT,id+step,0,MPI_COMM_WORLD,&status);
+                chunk = merge(chunk,s,other,m);
+                s = s+m;
+            }
+        }
+        else
+        {
+            int near = id-step;
+            MPI_Send(&s,1,MPI_INT,near,0,MPI_COMM_WORLD);
+            MPI_Send(chunk,s,MPI_INT,near,0,MPI_COMM_WORLD);
+            break;
+        }
+        step = step*2;
+    }
+*/
     for(){                  /* Ciclo encargado de regular las vueltas de ordenamiento, es decir, los diferentes niveles en el arbol
                                 construido progresivamente en la union de los vectores con los elementos ordenados, por cada proceso*/
-                                
-                                
+
+
         // se hace el receive del vector de elementos , o el envio del vector, luego se modifica el vector con etiquetas
         
         int bandera = 0;        // Indicador sobre el salto de etiquetas
-        for(int j = 0; j < etiTamano; ++j ){             /* Ciclo encargado de modificar el vector etiquetaProcesos, en donde se cambian las diferentes etiquetas                                dentro del mismo conforme avanza el proceso, indicando por medio de las etiquetas cuales procesos 
+        for(int j = 0; j < etiTamano; ++j ){             /* Ciclo encargado de modificar el vector etiquetaProcesos, en donde se cambian las diferentes etiquetas                                dentro del mismo conforme avanza el proceso, indicando por medio de las etiquetas cuales procesos
                                                             van a tomar un vector de elementos ordenado*/
             if(j = 0 || () ){
                 
@@ -102,8 +149,8 @@ int main(int argc, char **argv)
 
 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30      --   la cantidad de ciclos es n, 2^n = numElementos
-0 4 8 12 16 20 24 28
-0 8 16 24                                       -- brinco 1, 3, 7
+        0 4 8 12 16 20 24 28
+        0 8 16 24                                       -- brinco 1, 3, 7
 0 16
 0
 
