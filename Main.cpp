c7734746861a6ee082bedd488b2d44d03daea8c3
SlyJose <jpurena14@hotmail.com>
2015-03-26 14:50:30 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,19 +4,20 @@
   * Arquitectura de Computadoras
   * Tarea programada 1
   * @author Fabian Rodriguez B25695
-  * @author Jose Pablo Ureña B16692
+  * @author Jose Pablo UreÃ±a B16692
   * I Semestre 2015
   * El programa implementa el algoritmo de merge sort con paralelismo y utilizando MPI para el paso de mensajes
   */
 
+#include "OrdenadorElementos.h"
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <time.h>
 #include <stdlib.h>
 #include <string>
 #include "mpi.h"
-#include "ordenador.h"
 
 using namespace std;
 
@@ -53,6 +54,7 @@ int main(int argc, char **argv)
     int* vecMerge;                                      /* En este array se van a acomodar los numeros de cada proceso mas los que recibe del otro. */
     int cantActualProcs;                                /* La cantidad de procesos que le tocan a cada subproceso. */
     MPI_Status estado;                                  /* Para saber cual es el estado del proceso. */
+    int my_rank;					/* Indicador del proceso actual o identificador para el mismo*/
 
     MPI_Init(&argc, &argv);                                               /* Arranque del proceso MPI */
 
@@ -66,37 +68,35 @@ int main(int argc, char **argv)
         arrayDesorden = new int[numElementos];
         string numeros = "";
         int valor = 0;
+	stringstream ss;
         srand(time(NULL));
 
         ofstream numerosDesorden;                                           // Objeto Archivo
 
         numerosDesorden.open("ListaI.txt");                                 // Se crea el archivo que contiene los numeros en desorden
-        numerosDesorden << "Números en desorden: " << endl;
+        numerosDesorden << "NÃºmeros en desorden: " << endl;
 
         for(int i = 0; i < numElementos; ++i){                                   // Se generan todos los numeros random
             valor = (i* (rand()%10+1) );
             arrayDesorden[i] = valor;
-            numeros = to_string(valor);
+	    ss << valor;						// Se convierte el entero a string para guardarlo en el archivo
+	    numeros = ss.str();
             numerosDesorden << numeros+" " << endl;                     // Se llena el archivo con valores random
         }
-        
-        // ******** Preguntar si puede especificarse que un proceso reciba de solo una etiqueta
         
         MPI_Bcast(&tamVecTmp, 1, MPI_INT, 0, MPI_COMM_WORLD);   /* Se encarga de enviarle a cada proceso el numero de elementos que le tocan a ese proceso.*/
         vecTemporal = new int[tamVecTmp];
         MPI_Scatter(arrayDesorden, tamVecTmp, MPI_INT, vecTemporal, tamVecTmp, MPI_INT, 0, MPI_COMM_WORLD);  /* El proceso raiz envia a todos los procesos incluido el mismo, los elementos
                                                                                                                 a ordenar inicialmente, es decir, numElementos / numProcesos*/
-        metodoOrde.mergeSort(vecTempral, 0, tamVecTmp-1);       /* Ordena la parte que le corresponde al proceso,
+        metodoOrde.mergeSort(vecTemporal, 0, tamVecTmp-1);       /* Ordena la parte que le corresponde al proceso,
                                                                    esto es el proceso 0.*/
     }else{
         MPI_Bcast(&tamVecTmp, 1, MPI_INT, 0, MPI_COMM_WORLD);
         vecTemporal = new int[tamVecTmp];
-        MPI_Scatter(arrayDesorden, tamVecTmp, vecTemporal, tamVecTmp, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Scatter(arrayDesorden, tamVecTmp, MPI_INT, vecTemporal, tamVecTmp, MPI_INT, 0, MPI_COMM_WORLD);
         metodoOrde.mergeSort(vecTemporal, 0, tamVecTmp-1);  /* Ordena lo que le corresponde */
     }
-    
-    // aqui debe hacerse el merge en cada proceso
-    
+
     iteracion = 1;
     while(iteracion < numProcesos){
         if(my_rank%(2*iteracion) == 0){
@@ -118,30 +118,58 @@ int main(int argc, char **argv)
     }
 
     MPI_Finalize();
+
+    string respuesta;				/* Variable encargada de almacenar la respuesta del usuario sobre una pregunta especifica*/    
+
+    // llenar el archivo con los numeros ordenados
+
+
+	ofstream numerosOrden;                                           // Objeto Archivo
+	string numerosOrdenados;
+	stringstream ss2;						// Nuevo objeto stringstream para casting de valores enteros
+        numerosOrden.open("ListaF.txt");                                 // Se crea el archivo que contiene los numeros en orden
+        numerosOrden << "NÃºmeros en Orden: ";
+
+        for(int m = 0; m < numElementos; ++m){                                   /* Se escribe en el archivo todos los numeros en orden */
+	    //ss2 << ;								// Se convierte el entero a string para guardarlo en el archivo
+	    numerosOrdenados = ss2.str();
+            numerosOrden << numerosOrdenados+" " << endl;                     // Se llena el archivo con los valores ordenados
+	    if(m % 10 == 0){
+		numerosOrden << "\n";
+	    }
+        }
+
+
+
+    cout << "¿Desea observar los números generados aleatoriamente ordenados?(Y/N): ";
+    cin >> respuesta;
+
+    while( (respuesta != "Y") || (respuesta != "N") ){
+
+	    if(respuesta == "Y"){
+		cout << endl;
+		cout << "Los números ordenados son los siguientes: " << endl;
+		for(int f = 0; f < numElementos; ++f){			/* Se imprime el vector ordenado con los numeros*/
+			//cout << " " + vector[f] + " ";
+		}
+		cout << endl;
+		cout << "La lista de números ordenados se ha almacenado en el archivo: ListaF.txt" << endl;
+		cout << endl;
+	    }else{
+		if(respuesta == "N"){
+			cout << endl;
+			cout << "La lista de números ordenados se ha almacenado en el archivo: ListaF.txt" << endl;
+			cout << endl;
+		}else{
+			cout << endl;
+			cout << "No ha digitado una entrada válida, indique (Y/N): " <<endl;
+			cin >> respuesta;
+			cout << endl;
+		}
+	    }
+
+    } /* Fin del ciclo while que pregunta al usuario */
+	
+
     return 0;
 }
-
-/*
-0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30      --   la cantidad de ciclos es n, 2^n = numElementos
-0 4 8 12 16 20 24 28
-0 8 16 24                                       -- brinco 1, 3, 7
-0 16
-0
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
